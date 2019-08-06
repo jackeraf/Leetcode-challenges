905b011c291a343ba9561270dd49fd04b92294f1
jackeraf <jarekimar@yahoo.es>
2019-08-07 00:01:49 +0200

    length of longest substr

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,24 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
 
+function lengthOfLongestSubstring(s) {
+    let letters = {};
+    let start = 0;
+    let max = 1;
+
+    for(let i = 0; i < s.length; i++){
+        const letter = s[i];
+        let windowLength = i - start +1;
+        if(!(letter in letters)){
+            letters[letter] = i;
+            max = Math.max(max,windowLength);
+
+        }else{
+            start = letters[letter] + 1;
+            letters[letter] = i;
+        }
+    }
+    return max;
+}
 module.exports = lengthOfLongestSubstring;
