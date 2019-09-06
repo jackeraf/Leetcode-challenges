0d6a30c5f676e260b817cfe22dc79ffde4ef12ce
jackeraf <jarekimar@yahoo.es>
2019-09-06 21:53:16 +0200

    lc5

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,29 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+// time O(n^2) space O(1)
+// expand around the center technique
+
+function longestPalindrome(s) {
+  let maxLength = 1;
+  let startingWindow = 0;
+  function middleCompare(left, right) {
+    while (left >= 0 && right <= s.length && s[left] === s[right]) {
+      const currentWindowLength = right - left + 1;
+      if (currentWindowLength > maxLength) {
+        maxLength = currentWindowLength;
+        startingWindow = left;
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    middleCompare(i - 1, i + 1);
+    // for even string place the middle between left & right letters:
+    middleCompare(i, i + 1);
+  }
+  return s.slice(startingWindow, startingWindow + maxLength);
+}
 
 module.exports = longestPalindrome;
