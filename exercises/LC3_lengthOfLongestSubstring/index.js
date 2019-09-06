e0d5da8346de1eaa4f70450bebd6d4b2d0d3e795
jackeraf <jarekimar@yahoo.es>
2019-09-06 19:03:51 +0200

    longestsubstring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,24 +4,20 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-
 function lengthOfLongestSubstring(s) {
-    let letters = {};
-    let start = 0;
-    let max = 1;
-
-    for(let i = 0; i < s.length; i++){
-        const letter = s[i];
-        let windowLength = i - start +1;
-        if(!(letter in letters)){
-            letters[letter] = i;
-            max = Math.max(max,windowLength);
-
-        }else{
-            start = letters[letter] + 1;
-            letters[letter] = i;
-        }
+  let letters = {};
+  let max = 1;
+  let startingWindow = 0;
+  for (let i = 0; i < s.length; i++) {
+    const letter = s[i];
+    if (!(letter in letters)) {
+    } else {
+      startingWindow = letters[letter] + 1;
     }
-    return max;
+    letters[letter] = i;
+    let difference = i - startingWindow + 1;
+    max = Math.max(max, difference);
+  }
+  return max;
 }
 module.exports = lengthOfLongestSubstring;
