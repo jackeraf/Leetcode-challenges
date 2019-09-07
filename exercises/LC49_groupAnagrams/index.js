8df5513e5da0c230beea6a150d4a275af969206f
jackeraf <jarekimar@yahoo.es>
2019-09-07 14:07:15 +0200

    groupanagrams

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,21 @@
-function groupAnagrams(strs) {}
+// Time O(N K log K) where n is # of strings and k is length of strings
+// the sorting .sort does the k log k
+// space O(n k)
+
+function groupAnagrams(strs) {
+  let grouped = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    const sorted = strs[i]
+      .split("")
+      .sort()
+      .join("");
+    if (!(sorted in grouped)) {
+      grouped[sorted] = [];
+    }
+    grouped[sorted].push(strs[i]);
+  }
+  return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
