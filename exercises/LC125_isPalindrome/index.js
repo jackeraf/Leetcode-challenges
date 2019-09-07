9a2938629717f8eff5390fe7804acc6c90ad89f4
jackeraf <jarekimar@yahoo.es>
2019-09-07 14:46:14 +0200

    isPalindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,20 @@
-function isPalindrome(s) {}
+// Time O(n)
+// Space O(1) because sanitation will replace the s param
+
+function isPalindrome(s) {
+  s = s
+    .toLowerCase()
+    // regex [] to match all inside no matter the order of appearance
+    .replace(/[\W_]/g, "");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
