fb8c6055cfd4ad3ec2fbfb6e3d132c7c3306dc59
jackeraf <jarekimar@yahoo.es>
2019-09-07 21:39:12 +0200

    isAnagram

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,18 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  let sChar = {};
+
+  for (let i = 0; i < s.length; i++) {
+    const letter = s[i];
+    sChar[letter] = sChar[letter] + 1 || 1;
+  }
+  for (let i = 0; i < t.length; i++) {
+    const letter = t[i];
+
+    if (!sChar[letter]) return false;
+    sChar[letter]--;
+  }
+  return true;
+}
 
 module.exports = isAnagram;
