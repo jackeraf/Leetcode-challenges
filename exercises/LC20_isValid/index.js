8df82ce05506b93b8ebde504851131d7cf8a2d9d
jackeraf <jarekimar@yahoo.es>
2019-09-07 13:10:58 +0200

    validateParenthesis

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,16 @@
-const isValid = s => {};
+const isValid = s => {
+  let stack = [];
+  let pairHashMap = { "(": ")", "[": "]", "{": "}" };
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (pairHashMap[char]) {
+      stack.push(char);
+    } else if (pairHashMap[stack.pop()] !== char) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
 
 module.exports = isValid;
