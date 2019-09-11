95e16bf2ab63d89edf8b047d1b7ea137720fa307
jackeraf <jarekimar@yahoo.es>
2019-09-11 15:24:34 +0200

    climbingStairs O(1) space

--- exercises/LC70_climbStairs/index.js
+++ exercises/LC70_climbStairs/index.js
@@ -1,14 +1,29 @@
 // time O(n)
 // space 0(n)
 
+// const climbStairs = n => {
+//   if (3 >= n) return n;
+//   let ways = [0, 1, 2, 3];
+//   for (let i = 4; i <= n; i++) {
+//     let way = ways[i - 1] + ways[i - 2];
+//     ways.push(way);
+//   }
+//   return ways[n];
+// };
+
+// time O(n)
+// space O(1)
+
 const climbStairs = n => {
   if (3 >= n) return n;
-  let ways = [0, 1, 2, 3];
-  for (let i = 4; i <= n; i++) {
-    let way = ways[i - 1] + ways[i - 2];
-    ways.push(way);
+  let first = 1;
+  let second = 2;
+  for (let i = 3; i <= n; i++) {
+    let sum = second + first;
+    first = second;
+    second = sum;
   }
-  return ways[n];
+  return second;
 };
 
 module.exports = climbStairs;
