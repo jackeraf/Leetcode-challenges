5c1915043cca20f7b06ff2862c63aedbf2cac8a4
jackeraf <jarekimar@yahoo.es>
2019-09-11 14:45:50 +0200

    uniquePaths

--- exercises/LC62_uniquePaths/index.js
+++ exercises/LC62_uniquePaths/index.js
@@ -1,3 +1,38 @@
-function uniquePaths(m, n) {}
+function uniquePaths(cols, rows) {
+  let dpMatrix = [];
+
+  /*
+    [
+       [1,1,1,1],
+       [1,empty,empty,empty]
+    ]
+    
+    */
+
+  for (let i = 0; i < rows; i++) {
+    dpMatrix.push(new Array(cols));
+  }
+  // set 1st row cols values to 1
+  for (let i = 0; i < 1; i++) {
+    for (let j = 0; j < dpMatrix[i].length; j++) {
+      dpMatrix[i][j] = 1;
+    }
+  }
+  // set 1st col value from all rows to 1
+  for (let i = 0; i < dpMatrix.length; i++) {
+    dpMatrix[i][0] = 1;
+  }
+
+  // set unique uniquePaths
+
+  for (let i = 1; i < dpMatrix.length; i++) {
+    for (let j = 1; j < dpMatrix[i].length; j++) {
+      let sum = dpMatrix[i - 1][j] + dpMatrix[i][j - 1];
+      dpMatrix[i][j] = sum;
+    }
+  }
+
+  return dpMatrix[dpMatrix.length - 1][cols - 1];
+}
 
 module.exports = uniquePaths;
