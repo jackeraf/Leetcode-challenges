3184973a7068293a2294b638ce1aec064d92e262
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-19 11:47:44 +0200

    rob

--- exercises/LC198_rob/index.js
+++ exercises/LC198_rob/index.js
@@ -1,3 +1,18 @@
-function rob(nums) {}
+// time O(n)
+// space O(n)
+
+function rob(nums) {
+  if (nums.length === 0) return 0;
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+  let maxRobs = [nums[0], Math.max(nums[0], nums[1])];
+
+  for (let i = 2; i < nums.length; i++) {
+    const maxRob = Math.max(nums[i] + maxRobs[i - 2], maxRobs[i - 1]);
+    maxRobs.push(maxRob);
+  }
+  return maxRobs[maxRobs.length - 1];
+}
 
 module.exports = rob;
