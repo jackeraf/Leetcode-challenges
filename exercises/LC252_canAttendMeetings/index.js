850c2a5023c5fbce8ff23aa0bb6fb6d580c4f275
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-19 14:43:57 +0200

    meetingrooms

--- exercises/LC252_canAttendMeetings/index.js
+++ exercises/LC252_canAttendMeetings/index.js
@@ -4,6 +4,19 @@
 // canAttendMeetings([[0, 30], [5, 10], [15, 20]]) --> false
 // canAttendMeetings([[7, 10], [2, 4]]) --> true
 
-const canAttendMeetings = intervals => {};
+// time O(n log n)
+// space O(n)
+
+const canAttendMeetings = intervals => {
+  let startTimes = intervals.map(interval => interval[0]);
+  startTimes.sort((a, b) => a - b);
+  let endTimes = intervals.map(interval => interval[1]);
+  endTimes.sort((a, b) => a - b);
+
+  for (let i = 0; i < startTimes.length; i++) {
+    if (startTimes[i + 1] < endTimes[i]) return false;
+  }
+  return true;
+};
 
 module.exports = canAttendMeetings;
