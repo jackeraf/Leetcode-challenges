ed88eecbf3c37a3356e2a8c8551f25e3131b6878
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-19 12:42:05 +0200

    merge

--- exercises/LC56_merge/index.js
+++ exercises/LC56_merge/index.js
@@ -1,3 +1,22 @@
-const merge = intervals => {};
+// time O(n log n)
+// space O(n)
+
+const merge = intervals => {
+  if (!intervals.length) return intervals;
+
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res = [intervals[0]];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const currentInterval = intervals[i];
+    const lastInterval = res[res.length - 1];
+    if (lastInterval[1] >= currentInterval[0]) {
+      lastInterval[1] = Math.max(currentInterval[1], lastInterval[1]);
+    } else {
+      res.push(currentInterval);
+    }
+  }
+  return res;
+};
 
 module.exports = merge;
