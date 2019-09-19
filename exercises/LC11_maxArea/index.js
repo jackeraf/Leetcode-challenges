1bbf41b0e8fda6719ec1a4314a5a5752034101e7
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-09-19 16:28:03 +0200

    maxarea

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -1,3 +1,23 @@
-function maxArea(height) {}
+// time O(n)
+// space O(1)
+
+function maxArea(heights) {
+  let left = 0;
+  let right = heights.length - 1;
+  let maxArea = 0;
+
+  while (left < right) {
+    // to calculate the area we need to multiply the height by the width
+    const currentArea =
+      Math.min(heights[left], heights[right]) * (right - left);
+    maxArea = Math.max(currentArea, maxArea);
+    if (heights[right] > heights[left]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return maxArea;
+}
 
 module.exports = maxArea;
