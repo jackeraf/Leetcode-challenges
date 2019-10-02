2d5fe83a1a30a152fad22bb498df48ebbba18387
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-10-02 16:25:00 +0200

    maxProfit

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -1,3 +1,31 @@
-var maxProfit = function(prices) {};
+// time O(n^2)
+// space O(1)
+
+// var maxProfit = function(prices) {
+//   let max = -Infinity;
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       if (prices[i] < prices[j]) {
+//         let difference = prices[j] - prices[i];
+//         max = Math.max(difference, max);
+//       }
+//     }
+//   }
+//   return max === -Infinity ? 0 : max;
+// };
+
+// time O(n)
+// space O(1)
+
+const maxProfit = prices => {
+  let smallest = Infinity;
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    smallest = Math.min(smallest, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - smallest);
+  }
+  return maxProfit;
+};
 
 module.exports = maxProfit;
