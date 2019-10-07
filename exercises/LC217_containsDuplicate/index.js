bd3f97f7880904f24be609f01f59571a533e23ef
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-10-07 13:39:27 +0200

    containsDuplicate

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,18 @@
-const containsDuplicate = nums => {};
+// time O(n)
+// space O(unique(n)) and worst case O(n)
+
+const containsDuplicate = nums => {
+  const visitedNums = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (!(num in visitedNums)) {
+      visitedNums[num] = true;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = containsDuplicate;
