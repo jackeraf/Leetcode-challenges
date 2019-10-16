44b3c01c0958e45598ba008f449d6c13cdb31a6c
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-10-16 10:31:01 +0200

    invertTree

--- exercises/LC226_invertTree/index.js
+++ exercises/LC226_invertTree/index.js
@@ -1,3 +1,17 @@
-function invertTree(root) {}
+function invertTree(root) {
+  helper(root);
+  return root;
+}
+
+function helper(node) {
+  if (!node) return;
+
+  let temp = node.left;
+  node.left = node.right;
+  node.right = temp;
+
+  helper(node.left);
+  helper(node.right);
+}
 
 module.exports = invertTree;
