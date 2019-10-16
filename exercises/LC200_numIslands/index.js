e6d8e1b000bab8aa3f6e4033cd343ed9f2705036
Àlvarez Farell <jarek.alvarez-farell@capgemini.com>
2019-10-16 10:19:40 +0200

    numberOfIslands

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,3 +1,33 @@
-function numIslands(grid) {}
+function numIslands(grid) {
+  let counter = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid.length; col++) {
+      if (grid[row][col] === "1") {
+        counter++;
+        dfs(grid, row, col);
+      }
+    }
+  }
+
+  return counter;
+}
+
+function dfs(grid, row, col) {
+  if (
+    row < 0 ||
+    row >= grid.length ||
+    col < 0 ||
+    col >= grid[row].length ||
+    grid[row][col] === "0"
+  ) {
+    return;
+  }
+  grid[row][col] = "0";
+  dfs(grid, row - 1, col);
+  dfs(grid, row + 1, col);
+  dfs(grid, row, col - 1);
+  dfs(grid, row, col + 1);
+}
 
 module.exports = numIslands;
