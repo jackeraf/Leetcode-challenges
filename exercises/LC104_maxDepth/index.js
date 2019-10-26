3e83a8259311947cbd2cb8377c724be4f08cbec7
jackeraf <jarekimar@yahoo.es>
2019-10-26 11:35:28 +0200

    maxDepth

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,19 @@
-function maxDepth(root) {}
+// Time O(n)
+// Space O(1) if recursion counts as space complexity it would be O(n)
+
+function maxDepth(root) {
+  let max = 0;
+
+  function dive(node, currentDepth) {
+    if (!node) {
+      max = Math.max(currentDepth - 1, max);
+      return;
+    }
+    dive(node.left, currentDepth + 1);
+    dive(node.right, currentDepth + 1);
+  }
+  dive(root, 1);
+  return max;
+}
 
 module.exports = maxDepth;
