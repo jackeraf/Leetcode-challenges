f73638df4cd5deebbe6ebdd00e8009477a9f97fd
jackeraf <jarekimar@yahoo.es>
2019-10-26 13:11:00 +0200

    isSameTree

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -1,3 +1,24 @@
-function isSameTree(p, q) {}
+// Time O(p+q)
+// Space O(1)
+
+function isSameTree(p, q) {
+  let sameTree = true;
+
+  function compareNodes(p, q) {
+    if (!p && !q) return false;
+    if (!p || !q) {
+      sameTree = false;
+      return;
+    } else if (p.val !== q.val) {
+      sameTree = false;
+      return;
+    }
+
+    compareNodes(p.left, q.left);
+    compareNodes(p.right, q.right);
+  }
+  compareNodes(p, q);
+  return sameTree;
+}
 
 module.exports = isSameTree;
