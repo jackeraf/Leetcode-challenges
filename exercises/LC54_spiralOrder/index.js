176547d33dd235422cc2489e9bd857dc8537869a
jackeraf <jarekimar@yahoo.es>
2019-11-01 12:30:54 +0100

    spiralMatrix

--- exercises/LC54_spiralOrder/index.js
+++ exercises/LC54_spiralOrder/index.js
@@ -1,3 +1,39 @@
-function spiralOrder(matrix) {}
+function spiralOrder(matrix) {
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let direction = "right";
+  let output = [];
+
+  while (top <= bottom && left <= right) {
+    if (direction === "right") {
+      for (let i = left; i <= right; i++) {
+        output.push(matrix[top][i]);
+      }
+      top++;
+      direction = "down";
+    } else if (direction === "down") {
+      for (let i = top; i <= bottom; i++) {
+        output.push(matrix[i][right]);
+      }
+      right--;
+      direction = "left";
+    } else if (direction === "left") {
+      for (let i = right; i >= left; i--) {
+        output.push(matrix[bottom][i]);
+      }
+      bottom--;
+      direction = "up";
+    } else if (direction === "up") {
+      for (let i = bottom; i <= top; i++) {
+        output.push(matrix[i][left]);
+      }
+      left++;
+      direction = "right";
+    }
+  }
+  return output;
+}
 
 module.exports = spiralOrder;
