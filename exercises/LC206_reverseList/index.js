f10eb59d1b5a5765c99c7542a2493105ce97f2b2
jackeraf <jarekimar@yahoo.es>
2019-11-02 14:48:27 +0100

    reverseLinkedList

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,16 @@
-const reverseList = head => {};
+// time O(n)
+// space O(1)
+
+const reverseList = head => {
+  let current = head;
+  let prev = null;
+  while (current) {
+    let temp = current.next;
+    current.next = prev;
+    prev = current;
+    current = temp;
+  }
+  return prev;
+};
 
 module.exports = reverseList;
