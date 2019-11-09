4071af18ff6cdd3d8bc975c015042e95e98a8e86
jackeraf <jarekimar@yahoo.es>
2019-11-09 12:35:20 +0100

    removeNthFromEnd

--- exercises/LC19_removeNthFromEnd/index.js
+++ exercises/LC19_removeNthFromEnd/index.js
@@ -1,3 +1,21 @@
-function removeNthFromEnd(head, n) {}
+// time O(n)
+// space O(1)
+
+function removeNthFromEnd(head, n) {
+  // create a dummyhead for empty linked list or with 1 node to avoid multiple if statements
+  let dummyHead = { next: head };
+  let slow = dummyHead;
+  let fast = dummyHead;
+
+  for (let i = 1; i <= n; i++) {
+    fast = fast.next;
+  }
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  slow.next = slow.next.next;
+  return dummyHead.next;
+}
 
 module.exports = removeNthFromEnd;
