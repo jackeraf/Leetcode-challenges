cf961b280979d0d1437f95abfb1f4bbf821284bb
jackeraf <jarekimar@yahoo.es>
2019-11-09 12:57:04 +0100

    mergetwolists

--- exercises/LC21_mergeTwoLists/index.js
+++ exercises/LC21_mergeTwoLists/index.js
@@ -1,3 +1,23 @@
-function mergeTwoLists(l1, l2) {}
+// time O(min(N,M)) because the while loop will stop
+//  when the pointer of the smaller list points to null.
+// space O(1)
+
+function mergeTwoLists(l1, l2) {
+  let dummyHead = { next: null };
+  let current = dummyHead;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      current = current.next;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      current = current.next;
+      l2 = l2.next;
+    }
+  }
+  current.next = l1 || l2;
+  return dummyHead.next;
+}
 
 module.exports = mergeTwoLists;
