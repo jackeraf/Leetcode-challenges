57ebf2d629daa51a8b2a415831734f256ae58263
jackeraf <jarekimar@yahoo.es>
2019-11-09 12:02:06 +0100

    hasCycle

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -1,3 +1,18 @@
-function hasCycle(head) {}
+// time O(n)
+// space O(1)
+
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = hasCycle;
